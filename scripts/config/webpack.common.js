9e8fc0a0a1c2b41f50a54d6557dbf6ff1a6e0773
Dan Ye <dan.ye@Dans-MacBook-Pro.local>
2021-11-25 22:30:27 +0800

    add constant

--- scripts/config/webpack.common.js
+++ scripts/config/webpack.common.js
@@ -1,13 +1,14 @@
 const path = require('path');
+const { PROJECT_PATH } = require('../constant');
 
 module.exports = {
     // 入口文件是src下的app.js
     entry: {
-        app: path.resolve(__dirname, '../../src/app.js'),
+        app: path.resolve(PROJECT_PATH, './src/app.js'),
     },
     // 输出的文件位置为根目录下的dist
     output: {
         filename: 'js/[name].[hash:8].js',
-        path: path.resolve(__dirname, '../../dist'),
+        path: path.resolve(PROJECT_PATH, './dist'),
     },
 };
---
+++ scripts/constant.js
@@ -0,0 +1,10 @@
+const path = require('path');
+// __dirname是当前模块的目录名，/Users/dan.ye/Desktop/MyExercise/my-exercise/scripts
+// path.resolve()从右到左处理，构建的绝对路径为根目录
+const PROJECT_PATH = path.resolve(__dirname, '../');
+const PROJECT_NAME = path.parse(PROJECT_PATH).name;
+
+module.exports = {
+    PROJECT_PATH,
+    PROJECT_NAME
+};
