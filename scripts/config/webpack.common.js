55e6285eef66b8604aa3f6cf3eec30629b0a5f53
Dan Ye <dan.ye@Dans-MacBook-Pro.local>
2021-11-27 18:42:08 +0800

    add devtool

--- scripts/config/webpack.common.js
+++ scripts/config/webpack.common.js
@@ -3,14 +3,14 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { isDev, PROJECT_PATH } = require('../constant');
 
 module.exports = {
-    // 入口文件是src下的app.js
+    // 入口文件是src下的index.js
     entry: {
-        app: path.resolve(PROJECT_PATH, './src/app.js'),
+        app: path.resolve(PROJECT_PATH, './src/index.js'),
     },
     // 输出的文件位置为根目录下的dist
     output: {
         //  生产环境不能使用缓存版本，需要请求到最新文件
-        filename: `js/[name]${isDev ? '' : '.[hash:8]'}.js`,
+        filename: `js/[name]${isDev ? '' : '.[contenthash]'}.js`,
         path: path.resolve(PROJECT_PATH, './dist'),
     },
     plugins: [
@@ -20,17 +20,11 @@ module.exports = {
             cache: false, // 特别重要：防止之后使用v6版本 copy-webpack-plugin 时代码修改一刷新页面为空问题。
             minify: isDev ? false : {
                 removeAttributeQuotes: true,
-                collapseWhitespace: true,
-                removeComments: true,
                 collapseBooleanAttributes: true,
                 collapseInlineTagWhitespace: true,
-                removeRedundantAttributes: true,
-                removeScriptTypeAttributes: true,
-                removeStyleLinkTypeAttributes: true,
                 minifyCSS: true,
                 minifyJS: true,
                 minifyURLs: true,
-                useShortDoctype: true,
             },
         }),
     ]
--- scripts/config/webpack.dev.js
+++ scripts/config/webpack.dev.js
@@ -3,9 +3,8 @@ const common = require('./webpack.common');
 
 module.exports = merge(common, {
     mode: 'development',
+    devtool: 'eval-source-map', // 映射原始代码，帮助调试代码错误
     devServer: {
-        // stats: 'errors-only', // 终端仅打印 error
-        // clientLogLevel: 'silent', // 日志等级
         compress: true, // 是否启用 gzip 压缩
         open: true, // 打开默认浏览器
         hot: true, // 热更新
